44d9b610836c3b418523b6e908b637ffd44383bf
Gia Bảo <noffyns@outlook.com>
2021-07-01 11:36:34 +0700

    1/4 coming

---
+++ myproject/Controllers/CartController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using myproject.Models;
+using myproject.DAL;
+
+namespace myproject.Controllers
+{
+    public class CartController : Controller
+    {
+        private ProductContext db = new ProductContext();
+        List<Cart> cartlist = new List<Cart>();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        [HttpPost]
+        public ActionResult AddtoCart(Product product, int? amount)
+        {
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            Cart cart = new Cart(product, amount);
+            cartlist.Add(cart);
+            
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
