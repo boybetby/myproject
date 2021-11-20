f546104363e18f21f3ab2611d0c2cb98761ff356
Gia Bảo <noffyns@outlook.com>
2021-11-20 16:42:29 +0700

    changeinfo

--- myproject/Controllers/HomeController.cs
+++ myproject/Controllers/HomeController.cs
@@ -14,11 +14,100 @@ namespace myproject.Controllers
         {
             return View();
         }
-        public ActionResult User()
+        public ActionResult Users()
         {
+            if(Session["Id"] == null)
+            {
+                return RedirectToAction("Index");
+            }
             int id = (int)Session["Id"];
             User user = db.userModels.Find(id);
             return View(user);
         }
+        [HttpPost]
+        public ActionResult UserInfo(User userchange, string province, string district, string ward)
+        {
+            int id = (int)Session["Id"];
+            User user = db.userModels.Find(id);
+            user.FullName = userchange.FullName;
+            user.Email = userchange.Email;
+            user.PhoneNumber = userchange.PhoneNumber;
+            user.Address = userchange.Address;
+            user.Province = province;
+            user.District = district;
+            user.Ward = ward;
+            db.SaveChanges();
+            return RedirectToAction("Users");
+        }
+        [HttpPost]
+        public ActionResult Signup(String username, String fullname, String password, String confirmpassword)
+        {
+            User newUser = new User();
+            newUser.Username = username;
+            newUser.FullName = fullname;
+            newUser.Password = password;
+            newUser.Confirm = confirmpassword;
+
+            db.userModels.Add(newUser);
+
+            db.SaveChanges();
+
+            var checkUser = db.userModels.SingleOrDefault(u => u.Username == username && u.Password == password);
+            if (checkUser != null)
+            {
+                Session["Id"] = checkUser.Id;
+                Session["FullName"] = checkUser.FullName;
+                int id = (int)Session["Id"];
+                Session["InvalidUser"] = null;
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (username == null || password == null)
+            {
+                Session["InvalidUser"] = "Username or Password is empty";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var checkUser = db.userModels.SingleOrDefault(u => u.Username == username && u.Password == password);
+                if (checkUser != null)
+                {
+                    Session["Id"] = checkUser.Id;
+                    Session["FullName"] = checkUser.FullName;
+                    int id = (int)Session["Id"];
+                    Session["InvalidUser"] = null;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    Session["InvalidUser"] = "Invalid Username or Password";
+                    return RedirectToAction("Index");
+                }
+
+            }
+        }
+        public ActionResult Logout()
+        {
+            int id = (int)Session["Id"];
+            var user = db.userModels.Find(id);
+            if (user != null)
+            {
+                Session["Id"] = null;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Session["Id"] = null;
+                Session["Username"] = null;
+                return RedirectToAction("Index");
+            }
+        }
+        
     }
 }
